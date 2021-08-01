cd6b13dfcf26d23e9139bbee1bcf1c3ef1cfb7c2
Dimasik2001 <dimonkalibaba@gmail.com>
2021-08-01 20:14:04 +0300

    Пустой проект

---
+++ Modul3HW2/Modul3HW2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Modul3HW2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
